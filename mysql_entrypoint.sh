e9cc1979196280b0875095963d0fe9fa6c9536ae
kasra shirazi <kasthedeveloper@gmail.com>
2023-09-25 19:37:41 +0330

    done

--- mysql_entrypoint.sh
+++ mysql_entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+#!/bin/sh
+
+# Prompt for the MySQL root password interactively
+read -sp "Enter MySQL root password: " root
+
+# Import SQL file into MySQL using the provided password
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "SOURCE /world.sql;"
+
+
 # Import SQL file into MySQL
 #mysql -u root -proot -e "SOURCE /world.sql;"
